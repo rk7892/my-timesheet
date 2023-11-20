e11c5a7d709d5d517e36d8888c51cf87df47552b
hariharn12 <140273143+hariharn12@users.noreply.github.com>
2023-11-20 12:33:31 +0530

    includes projects.ts

--- src/app/pages/my-projects/my-projects.component.ts
+++ src/app/pages/my-projects/my-projects.component.ts
@@ -1,13 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import {MatTabsModule} from '@angular/material/tabs';
+
+
 
 @Component({
   selector: 'app-my-projects',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterLink, MatTabsModule],
   templateUrl: './my-projects.component.html',
   styleUrls: ['./my-projects.component.scss']
+  
 })
 export class MyProjectsComponent {
 
 }
+

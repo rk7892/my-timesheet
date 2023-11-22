e4c7cd3bbebbc0aba8751971490b05cbd468aeb0
rk7892 <ranjithraina7952@gmail.com>
2023-11-22 17:43:04 +0530

    c

--- src/app/pages/my-clients/clients-details-info-card/clients-details-info-card.component.ts
+++ src/app/pages/my-clients/clients-details-info-card/clients-details-info-card.component.ts
@@ -1,8 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-clients-details-info-card',
   standalone: true,
+  imports: [CommonModule, MatCardModule,MatButtonModule,MatMenuModule,MatIconModule, RouterLink],
   templateUrl: './clients-details-info-card.component.html',
   styleUrls: ['./clients-details-info-card.component.scss']
 })
--- src/app/pages/my-clients/clients-details-info/clients-details-info.component.ts
+++ src/app/pages/my-clients/clients-details-info/clients-details-info.component.ts
@@ -1,7 +1,8 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule,  } from '@angular/common';
+import { Component, OnDestroy, inject } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Subscription, filter } from 'rxjs';
+import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { ClientsDetailsInfoCardComponent } from '../clients-details-info-card/clients-details-info-card.component';
 import { ClientsDetailsCurrentProjectComponent } from '../clients-details-current-project/clients-details-current-project.component';
 import { ClientsDetailsCompletedProjectComponent } from '../clients-details-completed-project/clients-details-completed-project.component';
@@ -23,12 +24,35 @@ import { ClientsDetailsCompletedProjectComponent } from '../clients-details-comp
 
   ]
 })
-export class ClientsDetailsInfoComponent {
+export class ClientsDetailsInfoComponent implements OnDestroy{
+
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  private subscriptions = new Subscription();
 
   links = [
     { label: 'Current Project', link: 'clients-details-current-project'},
     { label: 'Completed Project', link: 'clients-details-completed-project'},
   ];
   lastActiveTab = this.links[0].link;
+tabs: any;
+
+  constructor() {
+    this.subscriptions.add(
+      this.router.events.pipe(filter(x => x instanceof NavigationEnd)).subscribe(() => {
+        if (this.route.children.length > 0) {
+          const [route] = this.route.children;
+          this.lastActiveTab = route.snapshot.url[0].path;
+          // fetch data based on the tab selected
+        } else {
+          this.router.navigate([this.links[0].link], { relativeTo: this.route })
+        }
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 
 }

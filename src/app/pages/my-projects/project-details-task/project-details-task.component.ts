a3f7e4719ab4aa6b5ed83e1fcc11f4a0f670c5d8
hariharn12 <140273143+hariharn12@users.noreply.github.com>
2023-11-22 19:51:30 +0530

    create a acordain

--- src/app/pages/my-projects/project-details-task/project-details-task.component.ts
+++ src/app/pages/my-projects/project-details-task/project-details-task.component.ts
@@ -1,15 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatTabsModule } from '@angular/material/tabs';
+import { RouterLink } from '@angular/router'
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-project-details-task',
   standalone: true,
-  imports: [CommonModule, RouterLink, ReactiveFormsModule],
+  imports: [CommonModule, RouterLink,MatTabsModule, ReactiveFormsModule,MatDialogModule,MatExpansionModule,MatFormFieldModule],
   templateUrl: './project-details-task.component.html',
   styleUrls: ['./project-details-task.component.scss']
 })
 export class ProjectDetailsTaskComponent {
 
-}
+  panelOpenState = false;
+  customCollapsedHeight: string = '80px';
+  customExpandedHeight: string = '80px';
+
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  isLinear = false;
+  constructor(private _formBuilder: FormBuilder,public dialog: MatDialog) {}
+ 
+  }
+
+

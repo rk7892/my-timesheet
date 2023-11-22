38ec22864acd92db4f136538b0bc0c9bf339ea19
rk7892 <ranjithraina7952@gmail.com>
2023-11-22 12:41:10 +0530

    t

--- src/app/pages/my-clients/clients-details-info/clients-details-info.component.ts
+++ src/app/pages/my-clients/clients-details-info/clients-details-info.component.ts
@@ -1,11 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { ClientsDetailsInfoCardComponent } from '../clients-details-info-card/clients-details-info-card.component';
+import { ClientsDetailsCurrentProjectComponent } from '../clients-details-current-project/clients-details-current-project.component';
+import { ClientsDetailsCompletedProjectComponent } from '../clients-details-completed-project/clients-details-completed-project.component';
 
 @Component({
   selector: 'app-clients-details-info',
   standalone: true,
   templateUrl: './clients-details-info.component.html',
-  styleUrls: ['./clients-details-info.component.scss']
+  styleUrls: ['./clients-details-info.component.scss'],
+  imports: [
+    CommonModule,
+    MatTabsModule,
+    RouterLink,
+    RouterOutlet,
+    RouterLinkActive,
+    ClientsDetailsInfoCardComponent,
+    ClientsDetailsCurrentProjectComponent,
+    ClientsDetailsCompletedProjectComponent
+
+  ]
 })
 export class ClientsDetailsInfoComponent {
+
+  links = [
+    { label: 'Current Project', link: 'clients-details-current-project'},
+    { label: 'Completed Project', link: 'clients-details-completed-project'},
+  ];
+  lastActiveTab = this.links[0].link;
 
 }

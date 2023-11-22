98e86269003dbe7283ae77a5d94e195cd3f03fc2
hariharn12 <140273143+hariharn12@users.noreply.github.com>
2023-11-22 17:43:11 +0530

    creating a project details task

---
+++ src/app/pages/my-projects/project-details-team/project-details-team.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-details-team',
+ 
+
+  templateUrl: './project-details-team.component.html',
+  styleUrls: ['./project-details-team.component.scss']
+})
+export class ProjectDetailsTeamComponent {
+
+}

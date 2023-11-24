de4bd11b5e6a95434e1ee87c647a53db1aec1a88
rk7892 <ranjithraina7952@gmail.com>
2023-11-24 09:48:52 +0530

    r

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
+  imports: [CommonModule, MatButtonModule, MatMenuModule, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-
+  @Input() isMobile = true;
+  @Output() toggleSideMenu = new EventEmitter();
 }
--- src/app/components/side-nav/side-nav.component.ts
+++ src/app/components/side-nav/side-nav.component.ts
@@ -1,15 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
 import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-side-nav',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule,RouterModule, MatMenuModule],
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.scss']
 })
 export class SideNavComponent {
-
+  @Input() isMobile: boolean = false;
+  @Input() show: boolean = true;
+  constructor() {}
 }

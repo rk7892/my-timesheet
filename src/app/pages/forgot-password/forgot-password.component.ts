375a7814cfc1f58733b06a9c06c4b840a52c0031
dinesh6369@ <dineshkumar636905@gmail.com>
2023-11-28 12:24:18 +0530

    forgot password

--- src/app/pages/forgot-password/forgot-password.component.ts
+++ src/app/pages/forgot-password/forgot-password.component.ts
@@ -1,9 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.scss']
+  styleUrls: ['./forgot-password.component.scss'],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
 })
 export class ForgotPasswordComponent {
 
